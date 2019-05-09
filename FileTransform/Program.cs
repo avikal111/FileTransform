e619037038e37b750b1b5c2230702d8aa94b62da
Rai <Avikal.Rai@thomsonreuters.com>
2019-05-09 11:10:12 +0530

    first commit

---
+++ FileTransform/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+namespace FileTransform
+{
+    class Program
+    {
+        private static string _inputFolder;
+        private static string _outputFolder;
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Wrong number of arguments. Please provide input folder path and output folder path separated by space as arguments.");
+                return;
+            }
+                
+
+            _inputFolder = args[0];
+            _outputFolder = args[1];
+
+
+            DirectoryInfo inf = new DirectoryInfo(_inputFolder);
+            Directory.CreateDirectory(_outputFolder);
+
+            var zipTool = new ZipTool();
+            foreach (FileInfo file in inf.GetFiles("*.gz"))
+
+            {
+                zipTool.Decomp(file, _outputFolder);
+
+                string curFile = file.FullName;
+                string origName = curFile.Remove(curFile.Length - file.Extension.Length);
+                string newPath = _outputFolder + "\\" + Path.GetFileName(origName);
+
+                if (File.Exists(newPath))
+                {
+                    EditFile(newPath);
+                    zipTool.CompressBack(newPath);
+                }
+            }
+        }
+
+        private static void EditFile(string newPath)
+        {
+            string xmlData = File.ReadAllText(newPath);
+            var replacedData = xmlData.Replace("\"D:", "\"C:").Replace("\"d:", "\"C:");
+            File.WriteAllText(newPath, replacedData);
+
+        }
+    }
+}
---
+++ FileTransform/ZipTool.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace FileTransform
+{
+    class ZipTool
+    {
+        public void Decomp(FileInfo fi, string outputFolderPath)
+
+        {
+
+            long i = 0; long c;
+
+            using (FileStream inFile = fi.OpenRead())
+            {
+                string curFile = fi.FullName;
+                string origName = curFile.Remove(curFile.Length - fi.Extension.Length);
+                string newPath = outputFolderPath + "\\" + Path.GetFileName(origName);
+
+                if (File.Exists(newPath))
+                    File.Delete(newPath);
+
+                using (FileStream outFile = File.Create(newPath))
+                {
+                    using (GZipStream Dc = new GZipStream(inFile, CompressionMode.Decompress))
+                    {
+
+                        byte[] buffer = new byte[10240];
+                        int numRead;
+
+                        while ((numRead = Dc.Read(buffer, 0, buffer.Length)) != 0)
+                        {
+
+                            outFile.Write(buffer, 0, numRead);
+                            c = (numRead + i);
+                        }
+
+                        Console.WriteLine("Decompressed: {0}", fi.Name);
+                    }
+                }               
+            }
+        }
+
+        public void CompressBack(string newPath)
+        {
+            var fileToCompress = new FileInfo(newPath);
+            using (FileStream originalFileStream = new FileInfo(newPath).OpenRead())
+            {
+                if ((File.GetAttributes(fileToCompress.FullName) &
+                   FileAttributes.Hidden) != FileAttributes.Hidden & fileToCompress.Extension != ".gz")
+                {
+                    using (FileStream compressedFileStream = File.Create(fileToCompress.FullName + ".gz"))
+                    {
+                        using (GZipStream compressionStream = new GZipStream(compressedFileStream,
+                           CompressionMode.Compress))
+                        {
+                            originalFileStream.CopyTo(compressionStream);
+
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Compressed: {0}", fileToCompress.Name);
+            File.Delete(newPath);
+        }
+    }
+}
